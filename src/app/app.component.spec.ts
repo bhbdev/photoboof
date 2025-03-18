0cad791559c1354ceb39af909c08180221dffbf0
beau <bhbdev@gmail.com>
2025-03-17 23:04:19 -0400

    chore:test updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,48 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let elem: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+    elem = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'photoboof' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('photoboof');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Photo Boof');
+    expect(elem.querySelector('h1')?.textContent).toContain('Photo Boof');
   });
   
-  // more tests
-
-  // should render an app-booth element
   it('should render app-booth', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('app-booth')).toBeTruthy();
+    expect(elem.querySelector('app-booth')).toBeTruthy();
   });
 
   // should render a footer with a p element with a element with an ng-icon element
   it('should render footer', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('footer p a ng-icon')).toBeTruthy();
+    expect(elem.querySelector('footer p a ng-icon')).toBeTruthy();
   });
 
 });
--- src/app/booth/booth.component.spec.ts
+++ src/app/booth/booth.component.spec.ts
@@ -3,32 +3,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoothComponent } from './booth.component';
 
 describe('BoothComponent', () => {
-  let component: BoothComponent;
-  let fixture: ComponentFixture<BoothComponent>;
+    let component: BoothComponent;
+    let fixture: ComponentFixture<BoothComponent>;
+    let getUserMediaMock: any;
+    
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BoothComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        const mockStream = new MediaStream();
+        getUserMediaMock = jasmine.createSpy('getUserMedia').and.returnValue(Promise.resolve(mockStream))
+        
+        spyOn(navigator.mediaDevices, 'getUserMedia').and.returnValue(getUserMediaMock);
 
-    fixture = TestBed.createComponent(BoothComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        await TestBed.configureTestingModule({
+            imports: [BoothComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(BoothComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    afterEach(() => {
+        // Reset the navigator.mediaDevices.getUserMedia mock
+        getUserMediaMock.calls.reset();
+    });
 
-  // Add more tests...
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  // should contain a video element
-  it('should contain a video element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('video')).toBeTruthy();
-  });
-    // Test if the component initializes with the correct default values
+    it('should contain a video element', () => {
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('video')).toBeTruthy();
+    });
+
     it('should initialize with default values', () => {
         expect(component.timer.value).toBe(3);
         expect(component.timer.active).toBeFalse();
@@ -38,7 +45,6 @@ describe('BoothComponent', () => {
         expect(component.isConnected).toBeFalse();
     });
 
-    // Test if the connect method sets isConnected to true
     it('should set isConnected to true when connect is called', async () => {
         const cameraServiceSpy = spyOn(component['_cameraService'], 'connect').and.returnValue(Promise.resolve());
         const isConnectedSpy = spyOn(component['_cameraService'], 'isConnected').and.returnValue(true);
@@ -49,7 +55,6 @@ describe('BoothComponent', () => {
         expect(component.isConnected).toBeTrue();
     });
 
-    // Test if the resetTimer method resets the timer correctly
     it('should reset the timer when resetTimer is called', () => {
         component.timer = { id: setInterval(() => {}, 1000), value: 1, active: true };
         component.resetTimer();
@@ -58,7 +63,6 @@ describe('BoothComponent', () => {
         expect(component.timer.id).toBeNull();
     });
 
-    // Test if the resetState method resets the state correctly
     it('should reset the state when resetState is called', () => {
         component.images = ['image1', 'image2'];
         component.shutterCount = 2;
@@ -72,19 +76,16 @@ describe('BoothComponent', () => {
         expect(component.timer.id).toBeNull();
     });
 
-    // TODO: fix if possible issue is likely with the cameraService.snapshot method
-    // Test if the takeSnapshot method updates images array
-    // it('should update images array when takeSnapshot is called', () => {
-    //     const snapshotSpy = spyOn(component['_cameraService'], 'snapshot').and.callFake((canvas, callback) => {
-    //         callback('imageData', null);
-    //     });
+    it('should update images array when takeSnapshot is called', () => {
+      const snapshotSpy = spyOn(component['_cameraService'], 'snapshot').and.callFake((canvas, callback) => {
+        callback('imageData', null);
+      });
 
-    //     component.takeSnapshot();
-    //     expect(snapshotSpy).toHaveBeenCalled();
-    //     expect(component.images[component.curCanvas]).toBe('imageData');
-    // });
+      component.takeSnapshot();
+      expect(snapshotSpy).toHaveBeenCalled();
+      expect(component.images[0]).toBe('imageData');
+    });
 
-    // Test if the changeFilter method applies the correct filter
     it('should apply the correct filter when changeFilter is called', () => {
         const event = { target: { id: 'filter2' } };
         component.changeFilter(event);
@@ -95,14 +96,12 @@ describe('BoothComponent', () => {
         });
     });
 
-    // Test if the showPrint method calls the print service
     it('should call the print service when showPrint is called', () => {
         const printSpy = spyOn(component['_printService'], 'generatePhotoStrip');
         component.showPrint();
         expect(printSpy).toHaveBeenCalledWith(component.photostrip.nativeElement, component['_printService'].print);
     });
 
-    // Test if the showDownload method calls the download service
     it('should call the download service when showDownload is called', () => {
         const downloadSpy = spyOn(component['_printService'], 'generatePhotoStrip');
         component.showDownload();
